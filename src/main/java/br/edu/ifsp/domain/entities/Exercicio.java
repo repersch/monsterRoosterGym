aa5cbd5129d28d69ab7feff3d80494fdb449121a
João Domingues <jmmarao@gmail.com>
2022-05-08 15:37:27 -0300

    Implementação da Classe Exercicio e ENUM GrupoMuscular

--- src/main/java/br/edu/ifsp/domain/entities/Exercicio.java
+++ src/main/java/br/edu/ifsp/domain/entities/Exercicio.java
@@ -1,9 +1,23 @@
 package br.edu.ifsp.domain.entities;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Exercicio {
     private Integer id;
     private String nome;
     private GrupoMuscular grupoMuscular;
     private String descricao;
     private Boolean emUso;
+
+    public Exercicio(String nome, GrupoMuscular grupoMuscular, String descricao, Boolean emUso) {
+        this.nome = nome;
+        this.grupoMuscular = grupoMuscular;
+        this.descricao = descricao;
+        this.emUso = emUso;
+    }
 }
