ffdfefa9fb608b50b45782ee5b7af3c7992cc26d
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2022-05-09 22:39:03 -0300

    create methods in class Treino

--- src/main/java/br/edu/ifsp/domain/entities/Treino.java
+++ src/main/java/br/edu/ifsp/domain/entities/Treino.java
@@ -7,4 +7,53 @@ public class Treino {
     private String nome;
     private String observacao;
     private List<ExercicioTreino> exerciciosTreino;
+
+    public Treino(Integer id, String nome, String observacao, List<ExercicioTreino> exerciciosTreino) {
+        this.id = id;
+        this.nome = nome;
+        this.observacao = observacao;
+        this.exerciciosTreino = exerciciosTreino;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public List<ExercicioTreino> getExerciciosTreino() {
+        return exerciciosTreino;
+    }
+
+    public void setExerciciosTreino(List<ExercicioTreino> exerciciosTreino) {
+        this.exerciciosTreino = exerciciosTreino;
+    }
+
+    @Override
+    public String toString() {
+        return "Treino{" +
+                "id=" + getId() +
+                ", nome='" + getNome() + '\'' +
+                ", observacao='" + getObservacao() + '\'' +
+                ", exerciciosTreino=" + getExerciciosTreino() +
+                '}';
+    }
 }
